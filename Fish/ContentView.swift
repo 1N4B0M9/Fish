be66744c6a288ac060da2b89713f92f37f0cc51f
Nathan Morelli <2005Nathan2005@gmail.com>
2023-01-18 08:53:29 -0500

    Initial Commit

---
+++ Fish/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fish
+//
+//  Created by Nathan Morelli (student LM) on 1/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fish/FishApp.swift
@@ -0,0 +1,17 @@
+//
+//  FishApp.swift
+//  Fish
+//
+//  Created by Nathan Morelli (student LM) on 1/18/23.
+//
+
+import SwiftUI
+
+@main
+struct FishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
